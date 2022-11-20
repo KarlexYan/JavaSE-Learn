b57b6e11e226ea1e184d09ed43c6b2e7c885f3e0
KarlexYan <117980958+KarlexYan@users.noreply.github.com>
2022-11-20 21:33:44 +0800

    Update Function Demo

---
+++ Self-Study/src/FunctionDemo/FunctionDemo3.java
@@ -0,0 +1,24 @@
+package FunctionDemo;
+
+import StreamDemo.StringOperation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FunctionDemo3 {
+    public static void main(String[] args) {
+        // 方法引用 引用成员方法
+
+
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list, "张无忌", "周芷若", "赵敏", "张强", "张三丰");
+
+        // 只要张开头，长度三个字
+        list.stream().filter(s -> s.startsWith("张")).filter(s -> s.length() == 3).forEach(s -> System.out.println(s));
+
+        System.out.println("============================================");
+
+        // 引用其他类的方法（自己写好的StringOperation)
+        list.stream().filter(new StringOperation()::stringJudge).forEach(s -> System.out.println(s));
+    }
+}
