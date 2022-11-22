8a53732511d21305422e78f18b8a1bbb2b3089ea
KarlexYan <KarlexYan@aliyun.com>
2022-11-22 19:28:26 +0800

    Update CodeInClass

---
+++ CodeInClass/Test/src/Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public int func(int x, int y) {
+        int result = 0;
+        if (x > 0) result = x + y / 2;
+        else result = x * 2;
+        return result;
+    }
+
+    public int max(int x, int y) {
+        if (x < y) return x;
+        else return y;
+
+    }
+
+    public int func2(int a) {
+        return 0;
+    }
+}
---
+++ CodeInClass/Test/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        System.out.println(demo.func(6, 4));
+
+
+    }
+}
